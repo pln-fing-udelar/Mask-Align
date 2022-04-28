2df1cc5b9327615e78da6e10aaf3c8e9a0542c0a
Santiago Goycoechea <sgoycoechea@fing.edu.uy>
2022-04-28 11:05:22 -0300

    Rename sentences files for alignment

--- scripts/generate-alignments/generate_alignments.sh
+++ scripts/generate-alignments/generate_alignments.sh
@@ -2,11 +2,11 @@
 set -ex
 python ./scripts/generate-alignments/remove_bad_rows.py
 python ./scripts/generate-alignments/generate_files.py
-spm_encode --model=en.model --output_format=piece < test.en > test.32k.en  
-spm_encode --model=es.model --output_format=piece < test.es > test.32k.es
+spm_encode --model=en.model --output_format=piece < sentences.en > sentences.32k.en  
+spm_encode --model=es.model --output_format=piece < sentences.es > sentences.32k.es
 python ./scripts/generate-alignments/process_answer_indexes.py
 mkdir -p corpus-es
-mv test.32k.en test.32k.es answers.en vocab.32k.es.txt vocab.32k.en.txt corpus-es/
+mv sentences.32k.en sentences.32k.es answers.en vocab.32k.es.txt vocab.32k.en.txt corpus-es/
 ./thualign/bin/generate.sh -s spanish -o output.txt
 spm_decode --model=es.model --input_format=piece < output.txt > output-plain.txt
 python ./scripts/generate-alignments/output_brackets_to_indexes.py
